bcb2e1c501e01bebbb23b5716ff677e832f36564
Alexandre Karpov <alexakarpov@gmail.com>
2019-04-18 19:46:19 -0400

    try with intuitive adjustments

--- lib/minimal_server/endoint.ex
+++ lib/minimal_server/endoint.ex
@@ -5,6 +5,9 @@ defmodule MinimalServer.Endpoint do
   plug(:match)
   IO.puts "after plug(:match)"
 
+  # now where do I put this?
+  forward(“/bot”, to: MinimalServer.Router) $ #does position matter?
+
   plug(Plug.Parsers,
     parsers: [:json],
     pass: ["application/json"],
@@ -16,4 +19,16 @@ defmodule MinimalServer.Endpoint do
   match _ do
     send_resp(conn, 404, "Requested page not found!")
   end
+
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]}
+    }
+  end
+
+  def start_link(_opts) do
+    Plug.Adapters.Cowboy2.http(__MODULE__, [])
+  end
+
 end
