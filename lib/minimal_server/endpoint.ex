cb5b00df2dc2e57e96effb752938eab37b4bcc41
Alexandre Karpov <alexakarpov@gmail.com>
2019-04-19 15:30:15 -0400

    extracted post body into vars

--- lib/minimal_server/endpoint.ex
+++ lib/minimal_server/endpoint.ex
@@ -1,9 +1,9 @@
 defmodule MinimalServer.Endpoint do
   use Plug.Router
 
-  #OK, so this module is a child of a Supervised application
+  #note: this is a child of a Supervised application
   
-  # any change to this file will cause only this module to recompile - while Router change will cause both modules to recompile, because this Endpoint module here depends on Router module
+  # any change to this file will cause only this module to recompile - while Router change will cause both modules to recompile, because this module here depends on Router module
   IO.puts "at the root of #{ __ENV__.file}:#{__ENV__.line}"
 
   plug(:match)
@@ -11,14 +11,12 @@ defmodule MinimalServer.Endpoint do
   plug(Plug.Parsers,
     parsers: [:json],
     pass: ["application/json"],
-    json_decoder: Poison,
-    foo: (IO.puts "lol" # this will also print once on compile
-          42)           # .. this becomes some config, I guess?
+    json_decoder: Poison
   )
 
   plug(:dispatch)
 
-  # this forward ( nothing to do with 302) must precede the next clause, otherwise:
+  # this forward (nothing to do with 302) must precede the next clause, otherwise:
   # warning: this clause cannot match because a previous clause at line 17 always matches
   forward("/bot", to: MinimalServer.Router) # dependency
   # .. but what if we had more than 1?
@@ -30,8 +28,9 @@ defmodule MinimalServer.Endpoint do
   end
 
   post "/events" do
-    IO.inspect conn.body_params # Prints JSON POST body
-    send_resp(conn, 200, "Success!")
+    IO.inspect conn
+    %{"hello" => msg, "number" => num} = conn.body_params
+    send_resp(conn, 200, "POST success!")
   end
 
   match _ do
