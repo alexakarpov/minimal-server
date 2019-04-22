8f98a6d0694c1d9496a8ea9fc4b6e4b4735b0847
Alexandre Karpov <alexakarpov@gmail.com>
2019-04-22 11:50:03 -0400

    use logger in app

--- lib/minimal_server/application.ex
+++ lib/minimal_server/application.ex
@@ -2,9 +2,10 @@ defmodule MinimalServer.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
+  require Logger
 
   use Application
-  
+
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
@@ -15,8 +16,7 @@ defmodule MinimalServer.Application do
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: MinimalServer.Supervisor]
-    IO.puts "#{__MODULE__} starting, getting up the children: " <> inspect children
-    IO.inspect children
+    Logger.debug "#{__MODULE__} starting, getting up the children: " <> inspect children
     Supervisor.start_link(children, opts)
   end
 end
