5b5795fe5f7d1d2d14d83d76f7ebc02367dff08b
Alexandre Karpov <alexakarpov@gmail.com>
2019-04-22 10:44:13 -0400

    remove default/generated module

--- test/minimal_server_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule MinimalServerTest do
-  use ExUnit.Case
-  doctest MinimalServer
-
-  test "greets the world" do
-    IO.puts "is this even running? yes"
-    assert MinimalServer.hello() == :world
-  end
-end
