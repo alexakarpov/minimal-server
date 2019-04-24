5571578e03465aafe103ae9fca015ea02177c076
Alexandre Karpov <alexakarpov@gmail.com>
2019-04-23 23:56:46 -0400

    more meaningful extension

--- test/machine_helper_test.exs
+++ test/machine_helper_test.exs
@@ -26,9 +26,9 @@ defmodule Machine.Helper.Tests do
       message = %{machine_id: 12,
                   timestamp: "2018-01-01T11:22:33Z",
                   type: "CycleComplete"}
-      {ok, journal} = File.open("/tmp/journal.txt", [:write, :read])
+      {:ok, journal} = File.open("/tmp/journal.log", [:write, :read])
       Machine.Helper.record_cycle_complete(journal, message)
-      {ok, txt} = File.read("/tmp/journal.txt")
+      {:ok, txt} = File.read("/tmp/journal.log")
       assert txt == "{\"type\":\"MachineCycled\",\"recorded_at\":\"2018-lalala\",\"machine_id\":12,\"event_time\":\"2018-01-01T11:22:33Z\"}\n"
       on_exit(fn -> File.rm("/tmp/journal.txt") end)
     end
